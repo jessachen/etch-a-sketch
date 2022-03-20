7e7817e6bb58b5e05f695a65d5fea17bbb327daa
jessachen <jessachen@berkeley.edu>
2022-03-19 20:11:38 -0600

    Created drawing hover effect using event listener.

--- javascript.js
+++ javascript.js
@@ -2,10 +2,21 @@ const container = document.querySelector('#container');
 
 for (i = 0; i < 16; i++) {
     const square = document.createElement('div');
+    square.setAttribute('id','square')
     square.style.width = '100px';
     square.style.height = '100px';
     square.style.backgroundColor = 'salmon';
     square.style.flex = '1 1 auto';
     container.appendChild(square);
 }
+
+const squares = document.querySelectorAll('#square');
+
+function draw() {
+    this.style.backgroundColor = 'black';
+}
+
+squares.forEach((square) => {
+    square.addEventListener('mouseenter', draw)
+})
 
