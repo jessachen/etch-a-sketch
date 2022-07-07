24dbee49656f28cafebf79a998c1f13ceb494faf
jessachen <jessachen@berkeley.edu>
2022-07-07 12:08:39 -0600

    Create button in index.html. Make multiple changes to javascript.js: add size variable, create clearGrid function, attach event listener to button to clear grid on click.

--- javascript.js
+++ javascript.js
@@ -1,22 +1,31 @@
 const container = document.querySelector('#container');
 
-for (i = 0; i < 16; i++) {
+let size = 4;
+
+for (i = 0; i < size * size; i++) {
     const square = document.createElement('div');
-    square.setAttribute('id','square')
+    square.setAttribute('id','square');
     square.style.width = '100px';
     square.style.height = '100px';
-    square.style.backgroundColor = 'salmon';
+    square.style.backgroundColor = 'pink';
     square.style.flex = '1 1 auto';
     container.appendChild(square);
 }
-
-const squares = document.querySelectorAll('#square');
 
 function draw() {
     this.style.backgroundColor = 'black';
 }
 
-squares.forEach((square) => {
-    square.addEventListener('mouseenter', draw)
-})
+container.childNodes.forEach((square) => {
+    square.addEventListener('mouseenter', draw);
+});
+
+function clearGrid() {
+   container.childNodes.forEach((square) => {
+       square.style.backgroundColor = 'pink';
+    });
+} 
+
+const button = document.getElementById('btn');
+button.addEventListener('click', clearGrid);
 
