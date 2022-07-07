fe301ce6e3c62959880fec0fa5bfb59da3379f3d
jessachen <jessachen@berkeley.edu>
2022-07-07 17:23:22 -0600

    Add range slider for selecting grid dimensions. Display grid size value above range slider bar. Change grid function to include calculation of invidual square dimensions.

--- javascript.js
+++ javascript.js
@@ -1,31 +1,61 @@
-const container = document.querySelector('#container');
+const grid = document.querySelector('#grid');
 
 let size = 4;
 
 for (i = 0; i < size * size; i++) {
     const square = document.createElement('div');
     square.setAttribute('id','square');
-    square.style.width = '100px';
-    square.style.height = '100px';
+    let numberDimension = 500 / size;
+    let stringDimension = numberDimension.toString().concat('px');
+    square.style.width = stringDimension;
+    square.style.height = stringDimension;
     square.style.backgroundColor = 'pink';
     square.style.flex = '1 1 auto';
-    container.appendChild(square);
+    grid.appendChild(square);
 }
 
 function draw() {
     this.style.backgroundColor = 'black';
 }
 
-container.childNodes.forEach((square) => {
+grid.childNodes.forEach((square) => {
     square.addEventListener('mouseenter', draw);
 });
 
 function clearGrid() {
-   container.childNodes.forEach((square) => {
+   grid.childNodes.forEach((square) => {
        square.style.backgroundColor = 'pink';
     });
 } 
 
 const button = document.getElementById('btn');
 button.addEventListener('click', clearGrid);
+
+const slider = document.getElementById('rangeSlider');
+const output = document.getElementById('sizeValue');
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = slider.value;
+}
+
+function newGrid () {
+   while (grid.firstChild) {
+       grid.removeChild(grid.firstChild)
+   }
+   
+    let size = slider.value;
+    for (i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.setAttribute('id','square');
+        let numberDimension = 500 / size;
+        let stringDimension = numberDimension.toString().concat('px');
+        square.style.width = stringDimension;
+        square.style.height = stringDimension;
+        square.style.backgroundColor = 'pink';
+        square.style.flex = '1 1 auto';
+        grid.appendChild(square);
+    }
+
+}
 
